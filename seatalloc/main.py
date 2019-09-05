b29b9213c96b6e7f3955ec88b4abdeb2709284f8
changwang23 <changwang23@gmail.com>
2019-09-05 22:12:46 +0800

    add main file

---
+++ seatalloc/main.py
@@ -0,0 +1,24 @@
+import json
+import predictor
+import allocate
+
+def predict_handler(event, context):
+    runcount = int(event['queryStringParameters']['rc'])
+    usercount = int(event['queryStringParameters']['uc'])
+    dayofyear = int(event['queryStringParameters']['doy'])
+    holidaycount = int(event['queryStringParameters']['doh'])
+
+    result = predictor.seat_count_predict(runcount, usercount, dayofyear, holidaycount)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(result)
+    }
+    
+def allocate_handler(event, context):
+    strategy = event['queryStringParameters']['startegy']
+
+    result = allocate.allocate_seat_for_N(strategy)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(result)
+    }
